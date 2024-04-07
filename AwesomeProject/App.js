087a18e6c8ddad50e5ef7c29e8fae463b76b3f37
amjvidal <136771834+amjvidal@users.noreply.github.com>
2024-04-07 19:33:34 -0300

    imput dados

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, TextInput, Button, StyleSheet } from "react-native";
 
 export default function App() {
+  const [input1, setInput1] = useState("");
+
+  const handleButtonPress = () => {
+    console.log("Valor do Input 1:", input1);
+    
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        
+        <TextInput
+          style={styles.input}
+          placeholder="Dados"
+          keyboardType="numeric"
+          value={input1}
+          onChangeText={setInput1}
+        />
+        <Button title="Executar" onPress={handleButtonPress} />
+      </View>
+    </>
   );
 }
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 20,
+  },
+  input: {
+    width: "100%",
+    height: 40,
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    paddingHorizontal: 10,
+  },
+});
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 20,
+  },
+  chartContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 20,
+  },
+  input: {
+    width: "100%",
+    height: 40,
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    paddingHorizontal: 10,
   },
 });
+
